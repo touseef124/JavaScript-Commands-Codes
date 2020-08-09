33336a394dfd164b5cb5acbba8baced5f4aa324c
Tauseef Ahmed Ansari <touseefansarizabr@gmail.com>
2020-08-09 19:19:11 +0500

    Let's push the JavaScript commands and codes

--- Chap-43-48.js
+++ Chap-43-48.js
@@ -45,3 +45,65 @@ function dec(){
     end = setInterval(decrease,500);}
 
 
+var students={};
+var count=0;
+function createobject(){
+    var naam=document.getElementById("name");
+    var num=document.getElementById("rank");
+    
+    var tr=document.createElement("tr");
+    var name=document.createTextNode(naam.value);
+    var rank=document.createTextNode(num.value);
+    
+
+    var th1=document.createElement("th");
+    th1.setAttribute("class","space")
+    var th2=document.createElement("th");
+    th1.appendChild(name);
+    th2.appendChild(rank);
+
+
+    var image2=document.createElement("img");
+    image2.setAttribute("src","delete.jpg");
+    image2.setAttribute("id","delete-logo");
+    image2.setAttribute("onclick","removeitem(this)")
+    
+
+    var image=document.createElement("img");
+    image.setAttribute("src","edit.jpg");
+    image.setAttribute("id","edit-logo");
+    image.setAttribute("onclick","edititem(this)")
+    
+
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(image2);
+    tr.appendChild(image);
+
+
+
+    var line=document.getElementById("list");
+    line.appendChild(tr);
+    naam.value="";
+    num.value="";
+    count++;
+   
+}
+function removeobject(){
+    var line=document.getElementById("list");
+    line.remove();
+}
+
+function removeitem(touseef){
+    touseef.parentNode.remove();
+    console.log(touseef.parentNode)
+}
+
+function edititem(touseef){
+    var name=prompt("Enter Name");
+    var rank=+prompt("Enter Rank");
+    touseef.parentNode.firstChild.innerHTML=name;
+    touseef.parentNode.childNodes[1].innerHTML=rank;
+    console.log(touseef.parentNode.firstChild.innerHTML);
+    console.log(touseef.parentNode.childNodes[1].innerHTML);
+}
